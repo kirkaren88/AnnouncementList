7cedbcb65702c796713d070278b03c44bf24f43b
Karen Kirakosyan <kirkaren88@yahoo.com>
2020-04-19 01:14:35 +0400

    Initial commit

---
+++ src/main/java/am/announcement/announcementlist/AnnouncementListApplication.java
@@ -0,0 +1,13 @@
+package am.announcement.announcementlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnouncementListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnnouncementListApplication.class, args);
+    }
+
+}
---
+++ src/test/java/am/announcement/announcementlist/AnnouncementListApplicationTests.java
@@ -0,0 +1,13 @@
+package am.announcement.announcementlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnouncementListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
